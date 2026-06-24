8afd36e975d6c582a0dec7df89044f20a3d0dfc4
LittleBitwise <fuzzy@techie.com>
2026-06-24 12:07:51 +0300

    Add select by item class

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from pprint import pp  # noqa
 import json
 
 ITEM_MODIFIERS = "./data/processed/item-modifiers.json"
+ITEM_TYPES = "./data/processed/item-types.json"
+
+with open(ITEM_MODIFIERS) as file:
+    item_modifiers = json.load(file)
+
+with open(ITEM_TYPES) as file:
+    item_types = json.load(file)
 
 ui.label("Add POE2 filter rule:")
 
 # Tab layout
 with ui.tabs() as tabs:
-    byName = ui.tab("By name")
-    byType = ui.tab("By type")
+    byName = ui.tab("By name")  # "Eye of Chayula"
+    byType = ui.tab("By base type")  # "Gold Amulet", "Vaal Gloves"
+    byClass = ui.tab("By item class")  # "Amulets", "Gloves"
 
 # Tab content
 with ui.tab_panels(tabs, value=byName):
@@ -19,20 +27,22 @@ with ui.tab_panels(tabs, value=byName):
             ui.checkbox("Partial match")
     with ui.tab_panel(byType):
         with ui.row():
+            # Todo: Would partial match conflict with select filter?
             ui.input("Base type")
             ui.checkbox("Partial match")
+    with ui.tab_panel(byClass):
+        with ui.row():
+            ui.select(item_types["Class"], with_input=True)
 
 
 # Todo: stats, amulet modifiers for now
-with open(ITEM_MODIFIERS) as file:
-    data = json.load(file)
 
 
 @ui.refreshable
 def statTierButtons() -> None:
     if not statSelect.value:
         return
-    x: list = data["Amulets"][statSelect.value]
+    x: list = item_modifiers["Amulets"][statSelect.value]
     tiers = len(x)
     with ui.button_group().bind_visibility_from(statSelect, "value"):
         for i in range(tiers, 0, -1):
@@ -40,7 +50,7 @@ def statTierButtons() -> None:
 
 
 statSelect = ui.select(
-    options=list(data["Amulets"].keys()),
+    list(item_modifiers["Amulets"].keys()),
     with_input=True,
     on_change=lambda e: statTierButtons.refresh(),
 )
